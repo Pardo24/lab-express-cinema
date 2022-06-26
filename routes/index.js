b5185e4058cf0522b5e981f20f5b331aa6f8b963
Dani Pardo <danielp.5@hotmail.com>
2022-06-26 23:49:09 +0200

    uf

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const { route } = require('../app');
 const router = express.Router();
 
 /* GET home page */
 router.get('/', (req, res, next) => res.render('index'));
+
+router.get('movies', (req, res, next)=> res.render("movies"))
+
+router.get('movies/:id', (req, res, next)=> res.send({}))
 
 module.exports = router;
